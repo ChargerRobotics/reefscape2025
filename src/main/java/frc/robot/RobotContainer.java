5dbcb47238cca1d18a87296a1f5adbf16bddd90f
DatAsianBoi123 <zach.su158@gmail.com>
2025-01-12 22:42:31 -0700

    created elevator subsystem and config

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,15 +10,18 @@ import com.datasiqn.robotutils.controlcurve.ControlCurve;
 import com.datasiqn.robotutils.controlcurve.ControlCurves;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.pkl.ElevatorConfig;
 import frc.robot.pkl.SwerveConfig;
 import frc.robot.subsystems.DriveTrainSubsystem;
+import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.swerve.SwerveDrive;
-import org.pkl.config.java.Config;
 import org.pkl.config.java.ConfigEvaluator;
 import org.pkl.core.ModuleSource;
 
 public class RobotContainer {
     private final DriveTrainSubsystem driveTrainSubsystem;
+    private final ElevatorSubsystem elevatorSubsystem;
+
     private final XboxController controller = new XboxController(0);
     private final Pigeon2 pigeon = new Pigeon2(20);
 
@@ -28,12 +31,14 @@ public class RobotContainer {
             .build();
 
     public RobotContainer() {
-        Config swerveConfig;
+        SwerveConfig swerveConfig;
+        ElevatorConfig elevatorConfig;
         try (ConfigEvaluator evaluator = ConfigEvaluator.preconfigured()) {
-            swerveConfig = evaluator.evaluate(ModuleSource.modulePath("swerve.pkl"));
+            swerveConfig = evaluator.evaluate(ModuleSource.modulePath("swerve.pkl")).as(SwerveConfig.class);
+            elevatorConfig = evaluator.evaluate(ModuleSource.modulePath("elevator.pkl")).as(ElevatorConfig.class);
         }
 
-        this.driveTrainSubsystem = new DriveTrainSubsystem(new SwerveDrive(swerveConfig.as(SwerveConfig.class)), () -> {
+        this.driveTrainSubsystem = new DriveTrainSubsystem(new SwerveDrive(swerveConfig), () -> {
             ChassisSpeeds speeds = new ChassisSpeeds(
                     driveCurve.get(-controller.getLeftY()),
                     driveCurve.get(-controller.getLeftX()),
@@ -41,5 +46,6 @@ public class RobotContainer {
             );
             return ChassisSpeeds.fromFieldRelativeSpeeds(speeds, pigeon.getRotation2d());
         });
+        this.elevatorSubsystem = new ElevatorSubsystem(elevatorConfig);
     }
 }
---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -0,0 +1,64 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkBase;
+import com.revrobotics.spark.SparkLowLevel;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.pkl.ElevatorConfig;
+import frc.robot.pkl.FRC;
+
+import java.util.List;
+
+public class ElevatorSubsystem extends SubsystemBase {
+    private final SparkMax controller;
+    private final ElevatorFeedforward feedforward;
+
+    private final TrapezoidProfile profile;
+
+    private TrapezoidProfile.State goal = new TrapezoidProfile.State();
+    private TrapezoidProfile.State setpoint = new TrapezoidProfile.State();
+
+    public ElevatorSubsystem(ElevatorConfig config) {
+        List<? extends FRC.MotorController> motors = config.motors;
+
+        this.controller = new SparkMax(motors.get(0).id, SparkLowLevel.MotorType.kBrushless);
+        for (int i = 1; i < motors.size(); i++) {
+            FRC.MotorController controllerConfig = config.motors.get(i);
+            SparkBaseConfig motorConfig = new SparkMaxConfig()
+                    .follow(this.controller, controllerConfig.inverted);
+            new SparkMax(controllerConfig.id, SparkLowLevel.MotorType.kBrushless)
+                    .configure(motorConfig, SparkBase.ResetMode.kNoResetSafeParameters, SparkBase.PersistMode.kNoPersistParameters);
+        }
+
+        ElevatorConfig.ElevatorFeedforwardConstants feedforwardConfig = config.feedforward;
+        this.feedforward = new ElevatorFeedforward(feedforwardConfig.kS, feedforwardConfig.kG, feedforwardConfig.kV, feedforwardConfig.kA);
+
+        ElevatorConfig.Constraints constraintsConfig = config.constraints;
+        this.profile = new TrapezoidProfile(new TrapezoidProfile.Constraints(constraintsConfig.maxVelocity, constraintsConfig.maxAcceleration));
+    }
+    public ElevatorSubsystem(SparkMax controller, ElevatorFeedforward feedforward, TrapezoidProfile profile) {
+       this.controller = controller;
+       this.feedforward = feedforward;
+       this.profile = profile;
+    }
+
+    public void setGoal(TrapezoidProfile.State goal) {
+        this.goal = goal;
+    }
+    public void setGoal(double goal) {
+        this.goal.position = goal;
+    }
+
+    @Override
+    public void periodic() {
+        TrapezoidProfile.State nextState = profile.calculate(TimedRobot.kDefaultPeriod, setpoint, goal);
+
+        controller.setVoltage(feedforward.calculateWithVelocities(setpoint.velocity, nextState.velocity));
+        setpoint = nextState;
+    }
+}
